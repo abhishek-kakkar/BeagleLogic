a4f657dfdef9f51e44ea6979bf52b5a2703fbceb
Kumar Abhishek <abhishek@theembeddedkitchen.net>
2014-06-21 05:44:56 +0700

    [PRU0FW Core] Cleanup & Enable halt-on-hostrequest
    The PRU firmware now aborts sampling and resets after
    receiving an ARM->host1 interrupt
    
    Signed-off-by: Kumar Abhishek <abhishek@theembeddedkitchen.net>

--- beaglelogic-firmware/beaglelogic-pru0-core.asm
+++ beaglelogic-firmware/beaglelogic-pru0-core.asm
@@ -8,11 +8,11 @@
 	.cdecls "beaglelogic-pru0.c"
 
 ;* C declaration:
-;* void run(struct capture_context *ctx)
+;* void run(struct capture_context *ctx, u32 trigger_flags)
 	.clink
 	.global run
 run:
-	LDI 	R17, (MAX_SG_ENTRIES * 8)
+	LDI	R17, (MAX_BUFLIST_ENTRIES * 8)
 	LDI	R0, 18
 	LDI	R1, 0
 $run$0:
@@ -22,35 +22,31 @@ $run$1:
 	; Load start and end address of mem chunk
 	; If it's zero, we're done
 	LBBO	&R18, R16, 0, 8
-	QBEQ	$run$exit, R18, 0
+	QBEQ	$run$canexit, R18, 0
 $run$2:
-	; Fill a buffer
-	WBS 	R31, 30
-	SBCO    &R0, C0, 0x24, 4
+	; Wait for and clear the buffer ready signal from PRU1
+	WBS	R31, 30
+	SBCO	&R0, C0, 0x24, 4
 
-	XIN 	10, &R21, 36
-	SBBO	&R21, R18, 0, 32
-
-	ADD 	R18, R18, 32
+	XIN	10, &R21, 36		; Get the logic data from PRU1
+	SBBO	&R21, R18, 0, 32	; Write buffer
+	ADD	R18, R18, 32
 	QBLT	$run$2, R19, R18
 
 	; Signal ARM that one buffer is now ready
 	; Also check if we received the kill signal
-	;ADD 	R1, R1, 1
-	;SBBO	&R1, R14, 8, 4
-	LDI 	R31, SYSEV_VR_PRU0_TO_ARM + 16
-	;QBBS	$run$exit, R31, 31
+	LDI	R31, SYSEV_VR_PRU0_TO_ARM + 16
+	QBBS	$run$exit, R31, 31
 
-	; Move to next SG chunk
-	ADD 	R16, R16, 8
+	; Move to next buffer
+	ADD	R16, R16, 8
 	QBLT	$run$1, R17, R16
 
-	JMP 	$run$0
-
-	LDI 	R14, 0
-	JMP 	$run$exit
+	LDI	R14, 0
+	JMP	$run$exit
+$run$canexit:
+	QBBS	$run$0, R15, 0
 $run$error:
-	MOV 	R14, R29
+	MOV	R14, R29
 $run$exit:
-	JMP R3.w2
-
+	JMP	R3.w2
