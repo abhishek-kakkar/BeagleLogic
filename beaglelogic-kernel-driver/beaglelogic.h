04ceddb220452ce96b397f026c8040232e490ec2
Kumar Abhishek <abhishek@theembeddedkitchen.net>
2014-06-22 04:21:13 +0700

    beaglelogic.h: add more ioctl calls and create enums for attrs
    Signed-off-by: Kumar Abhishek <abhishek@theembeddedkitchen.net>

--- beaglelogic-kernel-driver/beaglelogic.h
+++ beaglelogic-kernel-driver/beaglelogic.h
@@ -13,7 +13,7 @@
 #ifndef BEAGLELOGIC_H_
 #define BEAGLELOGIC_H_
 
-enum {
+enum beaglelogic_states {
 	STATE_BL_DISABLED,	/* Powered off (at module start) */
 	STATE_BL_INITIALIZED,	/* Powered on */
 	STATE_BL_MEMALLOCD,	/* Buffers allocated */
@@ -21,6 +21,16 @@ enum {
 	STATE_BL_RUNNING,	/* Data being captured */
 	STATE_BL_REQUEST_STOP,	/* Stop requested */
 	STATE_BL_ERROR   	/* Buffer overrun */
+};
+
+enum beaglelogic_triggerflags {
+	BL_TRIGGERFLAGS_ONESHOT = 0,
+	BL_TRIGGERFLAGS_CONTINUOUS
+};
+
+enum beaglelogic_sampleunit {
+	BL_SAMPLEUNIT_16_BITS = 0,
+	BL_SAMPLEUNIT_8_BITS
 };
 
 /* ioctl calls that can be issued on /dev/beaglelogic */
@@ -39,9 +49,14 @@ enum {
 #define IOCTL_BL_GET_CUR_INDEX      _IOR('k', 0x24, u32)
 #define IOCTL_BL_CACHE_INVALIDATE    _IO('k', 0x25)
 
-#define IOCTL_BL_FILL_TEST_PATTERN   _IO('k', 0x26)
+#define IOCTL_BL_GET_BUFFER_SIZE    _IOR('k', 0x26, u32)
+#define IOCTL_BL_SET_BUFFER_SIZE    _IOW('k', 0x26, u32)
 
-#define IOCTL_BL_START               _IO('k', 0x27)
-#define IOCTL_BL_STOP                _IO('k', 0x28)
+#define IOCTL_BL_GET_BUFUNIT_SIZE   _IOR('k', 0x27, u32)
+
+#define IOCTL_BL_FILL_TEST_PATTERN   _IO('k', 0x28)
+
+#define IOCTL_BL_START               _IO('k', 0x29)
+#define IOCTL_BL_STOP                _IO('k', 0x2A)
 
 #endif /* BEAGLELOGIC_H_ */
